fbfcc820ae6b2e85efa525c5193cba289de67907
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-05 18:03:56 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee ={name: `Sam`, streetAddress: `1250, Nairobi`};
+ 
+const updateEmployeeWithKeyAndValue = (employee, key = `streetaddress`, value = `11 Broadway`) => {
+    const updatedEmployee = employee;
+    for (const i in employee){
+        delete employee[i];
+    }
+    updatedEmployee.name = `Sam`;
+    const newEmployee = {...updatedEmployee, streetAddress: value}
+    return newEmployee;
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;
+    return employee;
+}
+
+const deleteFromEmployeeByKey = (employee, key) => {
+    employee[key] = `Sam`;
+    const newEmployee = {...employee, name: undefined}
+    return newEmployee;
+}
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+    return employee;
+}
